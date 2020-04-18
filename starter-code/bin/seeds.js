cb5d9dabdfa6bf87cd625d849db4a91004dfeca5
Andre R <andre.rabaca94@gmail.com>
2020-04-18 17:38:57 +0100

    Interaction 1 done

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+const Celebrity = require("../models/celebrity")
+
+const DB_Name = "celibrity-project"
+
+mongoose.connect("mongodb://localhost/{DB_NAME}",{
+useCreateIndex: true,
+useNewUrlParser: true,
+useUnifiedTopology: true
+})
+
+const celebrities = [
+    {
+        name:"Cristiano Ronaldo",
+        occupation:"Footballer",
+        catchPhrase:"SIIIIIIIII!!!"
+
+    },
+    {
+        name:"Marcelo Rebelo de Sousa",
+        occupation:"President",
+        catchPhrase:"Portugal acima de tudo"
+
+    },
+    {
+        name:"John Mayer",
+        occupation:"Musician",
+        catchPhrase:"Hopefully people can see my music is tethered to my brain"
+    }    
+]
+
+Celebrity.create(celebrities)
+.then(() =>{
+    console.log(`Created ${celebrities.length} celebrities`)
+    mongoose.connection.close()
+    }
+    )
+.catch(err=>{
+    console.log("Found an error creating the data on my Model")
+})
